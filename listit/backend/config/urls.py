441d92ce9f106b252ecc2011307da9dff050c745
gabrielbeloni <gabrielbelonibr@gmail.com>
2025-11-24 14:26:56 -0300

    create: table filmes

--- listit/backend/config/urls.py
+++ listit/backend/config/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from animes.views import AnimeViewSet
+from filmes.views import FilmeViewSet
+
 router = DefaultRouter()
 router.register(r'animes', AnimeViewSet)
+router.register(r'filmes', FilmeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)), 
+    path('api/', include(router.urls)),
 ]
---
+++ listit/backend/filmes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=200)
+    api_id = models.IntegerField(unique=True, help_text="ID do filme no TMDB")
+    capa_url = models.URLField(max_length=500, blank=True, null=True)
+    nota_pessoal = models.IntegerField(default=0, choices=[(i, str(i)) for i in range(1, 11)])
+    comentario = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.titulo} (Nota: {self.nota_pessoal})"
