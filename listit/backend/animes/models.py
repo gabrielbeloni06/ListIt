08cee5eaa1c1a0ed5d84059187b3f01a28e8349d
gabrielbeloni <gabrielbelonibr@gmail.com>
2025-11-24 14:13:08 -0300

    create: table database

--- listit/backend/animes/models.py
+++ listit/backend/animes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Anime(models.Model):
+    titulo = models.CharField(max_length=200)
+    api_id = models.IntegerField(unique=True, help_text="ID do anime no MyAnimeList/Jikan")
+    capa_url = models.URLField(max_length=500, blank=True, null=True)
+    nota_pessoal = models.IntegerField(default=0, choices=[(i, str(i)) for i in range(1, 11)])
+    comentario = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.titulo} - Nota: {self.nota_pessoal}"
