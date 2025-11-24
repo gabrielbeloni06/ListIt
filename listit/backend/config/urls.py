7f49781b994d817ddd915c8952624a5283d2cd4f
gabrielbeloni <gabrielbelonibr@gmail.com>
2025-11-24 17:06:22 -0300

    create: register backend

--- listit/backend/config/urls.py
+++ listit/backend/config/urls.py
@@ -3,10 +3,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from animes.views import AnimeViewSet
 from filmes.views import FilmeViewSet
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView
 
 router = DefaultRouter()
 router.register(r'animes', AnimeViewSet, basename='Anime')
@@ -17,4 +15,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', RegisterView.as_view(), name='auth_register'),
 ]
